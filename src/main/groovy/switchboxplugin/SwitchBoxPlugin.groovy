a0d9aa9a5a0d25f7b395108fb120083fea6888da
Chernov Svyatoslav <nomen.svyat@gmail.com>
2017-05-06 19:38:19 +0300

    Add plugin class

---
+++ src/main/groovy/switchboxplugin/SwitchBoxPlugin.groovy
@@ -0,0 +1,11 @@
+package switchboxplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SwitchBoxPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}

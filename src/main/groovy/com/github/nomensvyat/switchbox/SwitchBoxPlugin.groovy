9d107eac0a89884f06fe4d58f56f481ae0569f82
Chernov Svyatoslav <nomen.svyat@gmail.com>
2017-05-08 00:21:41 +0300

    Altogether

--- src/main/groovy/com/github/nomensvyat/switchbox/SwitchBoxPlugin.groovy
+++ src/main/groovy/com/github/nomensvyat/switchbox/SwitchBoxPlugin.groovy
@@ -1,7 +1,9 @@
 package com.github.nomensvyat.switchbox
 
+import com.android.build.gradle.AppExtension
 import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.LibraryPlugin
+import com.github.nomensvyat.switchbox.parser.JsonPropertyParser
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.logging.Logger
@@ -19,7 +21,40 @@ class SwitchBoxPlugin implements Plugin<Project> {
 
         project.extensions.create("switchBox", SwitchBoxExtension)
 
-        project.plugins.getPlugin()
+        project.afterEvaluate {
+            doAfterEvaluate(project)
+        }
+    }
+
+    def doAfterEvaluate(Project project) {
+        def appExtension = project.extensions.getByType(AppExtension)
+
+        def builder = BuildConfigFieldAdder.builder()
+        builder.addFieldContainer(FieldContainerFabric.createDefault(appExtension.defaultConfig))
+        appExtension.buildTypes.all {
+            builder.addFieldContainer(FieldContainerFabric.create(it))
+        }
+        appExtension.productFlavors.all {
+            builder.addFieldContainer(FieldContainerFabric.create(it))
+        }
+
+        def buildConfigFieldAdder = builder.build()
+
+        def switchBoxExtension = project.extensions.getByType(SwitchBoxExtension)
+        def file = getFile(project, switchBoxExtension.filePath)
+
+
+        def fieldMap = new JsonPropertyParser().parse(file)
+
+        buildConfigFieldAdder.compute(fieldMap)
+    }
+
+    static def getFile(Project project, String filePath) {
+        try {
+            return project.file(filePath)
+        } catch (Exception e) {
+            throw new Exception("File not found")
+        }
     }
 
     static def hasAndroidPlugin(Project project) {
